7dbdf10c0a47f70e11d3a6ba9455b1c7da761bd1
Brandon Graham <bgraham452@gmail.com>
2018-10-08 03:55:24 -0400

    Changed alt tags for my images to be ally-friendly

--- src/artwork.js
+++ src/artwork.js
@@ -10,18 +10,18 @@ class Artwork extends Component {
       <div className="artworks">
       <ul className="artwork-list-1">
       <li><img src={require('./images/artwork/4in.png')} className="artwork-img-1" alt="Mogami 4in cover art" /></li>
-      <li><img src={require('./images/artwork/album-shoot-1.png')} className="artwork-img-2" alt="Mogami 4in cover art" /></li>
-      <li><img src={require('./images/artwork/drawings-paintings.png')} className="artwork-img-3" alt="Mogami 4in cover art" /></li>
+      <li><img src={require('./images/artwork/album-shoot-1.png')} className="artwork-img-2" alt="Alicia Key's Here Album Cover Recreation" /></li>
+      <li><img src={require('./images/artwork/drawings-paintings.png')} className="artwork-img-3" alt="Various drawings and paintings" /></li>
       </ul>
       <ul className="artwork-list-2">
-      <li><img src={require('./images/artwork/album-shoot-2.png')} className="artwork-img-1" alt="Mogami 4in cover art" /></li>
-      <li><img src={require('./images/artwork/Mogami-Vice-Club.jpg')} className="artwork-img-2" alt="Mogami 4in cover art" /></li>
-      <li><img src={require('./images/artwork/school_daze.jpg')} className="artwork-img-3" alt="Mogami 4in cover art" /></li>
+      <li><img src={require('./images/artwork/album-shoot-2.png')} className="artwork-img-1" alt="The Weekend's Trilogy Album Cover Recreation" /></li>
+      <li><img src={require('./images/artwork/Mogami-Vice-Club.jpg')} className="artwork-img-2" alt="Mogami Vice Club promo art piece" /></li>
+      <li><img src={require('./images/artwork/school_daze.jpg')} className="artwork-img-3" alt="Alpha Week Flyer Compilation - University of Georgia Alpha Phi Alpha Chapter" /></li>
       </ul>
       <ul className="artwork-list-3">
-      <li><img src={require('./images/artwork/SUNGODS-twitter.jpg')} className="artwork-img-1" alt="Mogami 4in cover art" /></li>
-      <li><img src={require('./images/artwork/SON-GOKU.jpg')} className="artwork-img-2" alt="Mogami 4in cover art" /></li>
-      <li><img src={require('./images/artwork/album-shoot-3.png')} className="artwork-img-3" alt="Mogami 4in cover art" /></li>
+      <li><img src={require('./images/artwork/SUNGODS-twitter.jpg')} className="artwork-img-1" alt="Sun Gods" /></li>
+      <li><img src={require('./images/artwork/SON-GOKU.jpg')} className="artwork-img-2" alt="Son Goku homage piece" /></li>
+      <li><img src={require('./images/artwork/album-shoot-3.png')} className="artwork-img-3" alt="India Arie's Acoustic Soul Album Cover Recreation" /></li>
       </ul>
       </div>
       </section>
--- src/photography.js
+++ src/photography.js
@@ -9,19 +9,19 @@ class Photography extends Component {
       <h2 className="photography-title">Photography</h2>
       <div className="photos">
       <ul className="photography-list-1">
-      <li><img src={require('./images/photography/DSC_0064.jpg')} className="photography-img-1" alt="Mogami 4in cover art" /></li>
-      <li><img src={require('./images/photography/dt-after-dark-2.png')} className="photography-img-2" alt="Mogami 4in cover art" /></li>
-      <li><img src={require('./images/photography/coture-2.png')} className="photography-img-3" alt="Mogami 4in cover art" /></li>
+      <li><img src={require('./images/photography/DSC_0064.jpg')} className="photography-img-1" alt="Scene from the Bohemian Princess Shoot" /></li>
+      <li><img src={require('./images/photography/dt-after-dark-2.png')} className="photography-img-2" alt="Scene from BOLD's Downtown After Dark Shoot" /></li>
+      <li><img src={require('./images/photography/coture-2.png')} className="photography-img-3" alt="Scene from the Couture A La Cart Shoot" /></li>
       </ul>
       <ul className="photography-list-2">
-      <li><img src={require('./images/photography/flowers.jpg')} className="photography-img-1" alt="Mogami 4in cover art" /></li>
-      <li><img src={require('./images/photography/coture-1.jpg')} className="photography-img-2" alt="Mogami 4in cover art" /></li>
-      <li><img src={require('./images/photography/DSC_0175.jpg')} className="photography-img-3" alt="Mogami 4in cover art" /></li>
+      <li><img src={require('./images/photography/flowers.jpg')} className="photography-img-1" alt="Scene from UGA's North Campus Nature Shoot" /></li>
+      <li><img src={require('./images/photography/coture-1.jpg')} className="photography-img-2" alt="Scene from the Couture A La Cart Shoot" /></li>
+      <li><img src={require('./images/photography/DSC_0175.jpg')} className="photography-img-3" alt="Scene from Infusion Magazine's Spring 2018 Cover Shoot" /></li>
       </ul>
       <ul className="photography-list-3">
-      <li><img src={require('./images/photography/dt-after-dark-1.png')} className="photography-img-1" alt="Mogami 4in cover art" /></li>
-      <li><img src={require('./images/photography/infusion-shoot-1.jpg')} className="photography-img-2" alt="Mogami 4in cover art" /></li>
-      <li><img src={require('./images/photography/fifi.jpg')} className="photography-img-3" alt="Mogami 4in cover art" /></li>
+      <li><img src={require('./images/photography/dt-after-dark-1.png')} className="photography-img-1" alt="Scene from BOLD's Downtown After Dark Shoot" /></li>
+      <li><img src={require('./images/photography/infusion-shoot-1.jpg')} className="photography-img-2" alt="Scene from Infusion Magazine's Fall 2017 Cover Shoot" /></li>
+      <li><img src={require('./images/photography/fifi.jpg')} className="photography-img-3" alt="Scene from Fifi's Shoot. Part of the Free Shoots All Summer 16 Promotion" /></li>
       </ul>
       </div>
       </section>
